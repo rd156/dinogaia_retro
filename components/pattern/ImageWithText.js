d1e1367f4b56f6270d506e114ee4b4fb49e13c78
rd156 <hugo.ll@free.fr>
2024-12-04 18:47:48 +0100

    [ADD] ImageWithQuantity component

---
+++ components/pattern/ImageWithText.js
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import styles from './ImageWithText.module.css';
+
+const ImageWithText = ({ src, alt, quantity }) => {
+  return (
+    <div className={styles.container}>
+      <Image src={src} alt={alt} width={100} height={100} className={styles.image} />
+      <span className={styles.text}>{quantity}</span>
+    </div>
+  );
+};
+
+export default ImageWithText;
