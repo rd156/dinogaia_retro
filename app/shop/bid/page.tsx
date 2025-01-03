20e95489701d75ca3f100a877bc74e5da72a4cde
rd156 <hugo.ll@free.fr>
2025-01-03 16:52:53 +0100

    [ADD] page shop first

---
+++ app/shop/bid/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useLanguage } from "@/context/LanguageContext";
+import { translate, Loadtranslate } from "@/utils/translate";
+import { useSearchParams } from "next/navigation";
+import { API_URL } from "@/config/config";
+import "./page.css";
+import ButtonFancy from "@/components/pattern/ButtonFancy";
+import ButtonNeon from "@/components/pattern/ButtonNeon";
+
+const CavePage: React.FC = () => {
+  const searchParams = useSearchParams();
+  const { language, toggleLanguage } = useLanguage();
+  const [translations, setTranslations] = useState({});
+  const [resultData, setResultData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [message, setMessage] = useState("");
+
+  // Charger les traductions
+  useEffect(() => {
+    const fetchTranslations = async () => {
+    const loadedTranslations = await Loadtranslate(language, ["shop", "item", "global"]);
+    setTranslations(loadedTranslations);
+    };
+    fetchTranslations();
+  }, [language]);
+
+
+  return (
+    <main className="content">
+      <div className="content_top">
+        {errorMessage && (
+          <p className="alert-red">{errorMessage}</p>
+        )}
+        {message && (
+          <p className="alert-green">{message}</p>
+        )}
+        <div className="block_white">
+          blablabla
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default CavePage;
--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -30,15 +30,92 @@ const CavePage: React.FC = () => {
 
   return (
     <main className="content">
-      <div className="content_top"> 
+      <div className="content_top">
         {errorMessage && (
           <p className="alert-red">{errorMessage}</p>
         )}
         {message && (
           <p className="alert-green">{message}</p>
         )}
-        <div className="table-container">
-          
+        <div className="block_white">
+          <div style={{ display: "flex", justifyContent: "space-between", alignItems: "flex-start" }}>
+            <h1 style={{ width: "100%", textAlign: "center", border: "2px solid red", padding: "10px", display: "inline-block", fontSize: "18px", fontWeight: "bold", marginBottom: "20px" }}>
+              {translations.shop?.SHOP_CHOICE}
+            </h1>
+          </div>
+
+          <div className="blocks_container" style={{ display: "flex", justifyContent: "space-between", padding: "20px", height: "50vh"}}>
+            <div className="left_block" style={{ 
+              width: "45%", 
+              border: "2px solid gold", 
+              padding: "20px", 
+              textAlign: "center",
+              borderRadius: "10px", 
+              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+              transition: "transform 0.3s ease-in-out",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center"
+            }}>
+              <p style={{ fontSize: "18px", fontWeight: "bold", marginBottom: "20px" }}>{translations.shop?.DAILY_BID}</p>
+              <a href="/shop/bid" style={{ textDecoration: "none" }}>
+                <button style={{
+                  padding: "10px", 
+                  backgroundColor: "transparent", 
+                  border: "none", 
+                  borderRadius: "5px", 
+                  cursor: "pointer", 
+                  transition: "background-color 0.3s"
+                }}>
+                  <img 
+                    src="/bouton/bid.jpg"
+                    alt="Bid" 
+                    style={{
+                      width: "250px",        
+                      height: "250px",       
+                      objectFit: "contain"  
+                    }} 
+                  />
+                </button>
+              </a>
+            </div>
+            <div className="left_block" style={{ 
+              width: "45%", 
+              border: "2px solid gold", 
+              padding: "20px", 
+              textAlign: "center",
+              borderRadius: "10px", 
+              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+              transition: "transform 0.3s ease-in-out",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center"
+            }}>
+              <p style={{ fontSize: "18px", fontWeight: "bold", marginBottom: "20px" }}>{translations.shop?.SHOP_HDV}</p>
+              <a href="/shop/hdv" style={{ textDecoration: "none" }}>
+                <button style={{
+                  padding: "10px", 
+                  backgroundColor: "transparent", 
+                  border: "none", 
+                  borderRadius: "5px", 
+                  cursor: "pointer", 
+                  transition: "background-color 0.3s"
+                }}>
+                  <img 
+                    src="/bouton/hdv.jpg"
+                    alt="Bid" 
+                    style={{
+                      width: "250px",        
+                      height: "250px",       
+                      objectFit: "contain"  
+                    }} 
+                  />
+                </button>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </main>
