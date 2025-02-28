4e84fc08277cf4c7e54fce4803c48229dcabf27f
rd156 <hugo.ll@free.fr>
2025-02-28 23:45:22 +0100

    [FIRST] add parameter

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,66 +1,72 @@
 "use client";
 
 import { useEffect, useState, Fragment } from "react";
-import { useLanguage } from "@/context/LanguageContext";
 import { translate, Loadtranslate } from "@/utils/translate";
-import { useSearchParams } from "next/navigation";
 import { API_URL } from "@/config/config";
-import "./page.css";
-import Link from 'next/link';
-import ButtonFancy from "@/components/pattern/ButtonFancy";
-import ButtonNeon from "@/components/pattern/ButtonNeon";
+import "../page.css";
+import { useOption } from "@/context/OptionsContext";
 
-const QuestPage: React.FC = () => {
-  const { language, toggleLanguage } = useLanguage();
+const OptionPage: React.FC = () => {
   const [translations, setTranslations] = useState({});
-  const [loading, setLoading] = useState<boolean>(true);
   const [errorMessage, setErrorMessage] = useState("");
   const [message, setMessage] = useState("");
-  const [chapter, setChapter] = useState<number>(0);
+  const {option, updateImageTemplate, updateLanguage} = useOption();
 
-  // Charger les traductions
   useEffect(() => {
     const fetchTranslations = async () => {
-    const loadedTranslations = await Loadtranslate(language, ["story", "global"]);
-    setTranslations(loadedTranslations);
-    };
-    fetchTranslations();
-  }, [language]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      setErrorMessage("");
-      const token = localStorage.getItem('token');
-      try {
-        const response = await fetch(`${API_URL}/account/chapter_story`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-          },
-        });
-        
-        if (!response.ok) {
-          throw new Error('Erreur lors de la récupération des bid');
-        }
-
-        const chapterData = await response.json();
-        if (chapterData.chapter)
-        {        
-          setChapter(chapterData.chapter);
-        }
-      } catch (error) {
-        
-      } finally {
-        setLoading(false);
-      }
+      const loadedTranslations = await Loadtranslate(option?.language, ["setting", "global"]);
+      setTranslations(loadedTranslations);
     };
 
-    fetchData();
-  }, []);
+    if (option?.language) {
+      fetchTranslations();
+    }
+  }, [option?.language]);
 
+  const images_template = [
+    { name: 'reborn', label: translations.setting?.TEMPLATE_IMAGE_REBORN },
+    { name: 'retro', label: translations.setting?.TEMPLATE_IMAGE_RETRO },
+  ];
+  const languages = [
+    { lang: 'en', label: translations.setting?.LANGUAGE_ENGLISH },
+    { lang: 'fr', label: translations.setting?.LANGUAGE_FRENCH },
+  ];
+  const updateSetting = async (field: string, value: string) => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch(`${API_URL}/account/setting/update`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify({ [field]: value }),
+      });
   
+      const result = await response.json();
+      console.log("result")
+      console.log(result)
+      return result;
+    } catch (error) {
+      setErrorMessage(`Erreur lors de la mise à jour du ${field}`);
+      console.error(error);
+      return null;
+    }
+  };
+  const handleButtonImageClick = async (image_template: string) => {
+    const result = await updateSetting("image_template", image_template);
+    
+    if (result && result.image_template) {
+      updateImageTemplate(result.image_template);
+    }
+  };
+  
+  const handleButtonLanguageClick = async (template_language: string) => {
+    const result = await updateSetting("language", template_language);
+    if (result && result.language) {
+      updateLanguage(result.language);
+    }
+  };
   return (
     <main className="content">
       <div className="content_top">
@@ -71,11 +77,55 @@ const QuestPage: React.FC = () => {
           <p className="alert-green">{message}</p>
         )}
         <div className="block_white">
-          
+          <div style={{ display: 'flex', gap: '10px', justifyContent: 'center', alignItems: 'center' }}>
+            {languages.map(({ lang, label }) => (
+              <button
+                onClick={() => handleButtonLanguageClick(lang)}
+                key={lang}
+                style={{
+                  padding: '10px 20px',
+                  cursor: option?.language === lang ? 'default' : 'pointer',
+                  backgroundColor: option?.language === lang ? '#0070f3' : '#2f2f2f',
+                  color: 'white',
+                  border: 'none',
+                  borderRadius: '5px',
+                  opacity: option?.language === lang ? 1 : 0.6,
+                }}
+                aria-label={`Change language to ${label}`}
+                disabled={option?.language === lang}
+              >
+                {label}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        <div className="block_white">
+          <div style={{ display: 'flex', gap: '10px', justifyContent: 'center', alignItems: 'center' }}>
+            {images_template.map(({ name, label }) => (
+              <button
+                onClick={() => handleButtonImageClick(name)}
+                key={name}
+                style={{
+                  padding: '10px 20px',
+                  cursor: option?.image_template === name ? 'default' : 'pointer',
+                  backgroundColor: option?.image_template === name ? '#0070f3' : '#2f2f2f',
+                  color: 'white',
+                  border: 'none',
+                  borderRadius: '5px',
+                  opacity: option?.image_template === name ? 1 : 0.6,
+                }}
+                aria-label={`Change image to ${label}`}
+                disabled={option?.image_template === name}
+              >
+                {label}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </main>
   );
 };
 
-export default QuestPage;
+export default OptionPage;
--- context/OptionsContext.js
+++ context/OptionsContext.js
@@ -55,23 +55,23 @@ export const OptionProvider = ({ children }) => {
   const updateImageTemplate = (newImageTemplate) => {
     setOption((prevOption) => {
       const updatedOption = { ...prevOption, image_template: newImageTemplate };
-      localStorage.setItem("option", JSON.stringify(updatedOption)); // Met à jour localStorage
-      return updatedOption; // Retourne l'option mise à jour
+      localStorage.setItem("option", JSON.stringify(updatedOption));
+      return updatedOption;
     });
   };
   const updateLanguage = (newLanguage) => {
     setOption((prevOption) => {
       const updatedOption = { ...prevOption, language: newLanguage };
-      localStorage.setItem("option", JSON.stringify(updatedOption)); // Met à jour localStorage
-      return updatedOption; // Retourne l'option mise à jour
+      localStorage.setItem("option", JSON.stringify(updatedOption));
+      return updatedOption;
     });
   };
   
   const updateDisplayTemplate = (newDisplayTemplate) => {
     setOption((prevOption) => {
       const updatedOption = { ...prevOption, display_template: newDisplayTemplate };
-      localStorage.setItem("option", JSON.stringify(updatedOption)); // Met à jour localStorage
-      return updatedOption; // Retourne l'option mise à jour
+      localStorage.setItem("option", JSON.stringify(updatedOption));
+      return updatedOption;
     });
   };
   
