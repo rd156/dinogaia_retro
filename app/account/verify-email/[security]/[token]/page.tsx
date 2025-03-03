94c8d1f662b503343b650685ce8d05ab2c8b7990
rd156 <hugo.ll@free.fr>
2025-03-03 19:03:23 +0100

    [ADD] change email

---
+++ app/account/verify-email/[security]/[token]/page.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useOption } from "@/context/OptionsContext";
+import { useParams, useRouter } from "next/navigation";
+import { translate, Loadtranslate } from "@/utils/translate";
+import { useSearchParams } from "next/navigation";
+import { API_URL } from "@/config/config";
+import Link from "next/link";
+import "./page.css";
+
+const RencontrePage: React.FC = () => {
+  const params = useParams();
+  const searchParams = useSearchParams();
+  const [rencontreInfo, setRencontreInfo] = useState<any>(null);  // Pour stocker la rencontre
+  const [loading, setLoading] = useState<boolean>(true);
+  const [endRencontre, setEndrencontre] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [message, setMessage] = useState("");
+  const {option} = useOption();
+  const [translations, setTranslations] = useState({});
+
+  // Charger les traductions
+  useEffect(() => {
+    const fetchTranslations = async () => {
+      const loadedTranslations = await Loadtranslate(option?.language, ["settings", "global"]);
+      setTranslations(loadedTranslations);
+    };
+
+    if (option?.language) {
+      fetchTranslations();
+    }
+  }, [option?.language]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const token = localStorage.getItem("token");
+      try {
+        const caveResponse = await fetch(`${API_URL}/account/verify-email/${params.security}/${params.token}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const result = await caveResponse.json();
+        setMessage(result);
+        console.log(result)
+      } catch (error) {
+        
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <main className="content">
+      <div className="content_top">
+        <div className="block_white">
+          {translations.settings?.["CONFIRMATION_MAIL_" + message]}
+          <br />
+          <Link href="/cave">
+            <button
+              style={{
+                margin: "10px",
+                padding: "10px 20px",
+                fontSize: "16px",
+                cursor: "pointer",
+                backgroundColor: "#4CAF50",
+                color: "white",
+                border: "none",
+                borderRadius: "5px",
+              }}
+              >
+              {translations.settings?.GAME_RETURN}
+              </button>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default RencontrePage;
+
---
+++ app/settings/profil/page.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import { useOption } from "@/context/OptionsContext";
+import { Loadtranslate } from "@/utils/translate";
+import { useEffect, useState } from "react";
+import React from 'react';
+import { API_URL } from "@/config/config";
+import "./page.css";
+import ButtonFancy from "@/components/pattern/ButtonFancy";
+
+const ProfilePage: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [messageEmail, setMessageEmail] = useState("");
+  const [messagePseudo, setMessagePseudo] = useState("");
+  const { option } = useOption();
+  const [translations, setTranslations] = useState({});
+  const [advantages, setAdvantages] = useState({});
+  const [username, setUsername] = useState("");
+  const [pseudo, setPseudo] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    const fetchTranslations = async () => {
+      const loadedTranslations = await Loadtranslate(option?.language, ["settings", "global"]);
+      setTranslations(loadedTranslations);
+    };
+
+    if (option?.language) {
+      fetchTranslations();
+    }
+  }, [option?.language]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const token = localStorage.getItem("token");
+      try {
+        const profilResponse = await fetch(`${API_URL}/account/get_profile`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const profile_info = await profilResponse.json()
+        if (typeof profile_info === "object"){
+          setUsername(profile_info.username);
+          setAdvantages(profile_info.advantages);
+          setPseudo(profile_info.pseudo);
+          setEmail(profile_info.email);
+        }
+        console.log(profile_info)
+
+      } catch (error) {
+      } finally {
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleChangePseudo = (e) => {
+    setPseudo(e.target.value);
+  };
+
+  const handleSavePseudo = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await fetch(`${API_URL}/account/change-pseudo`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ pseudo }),
+      });
+
+      const change_pseudo = await response.json()
+      setMessagePseudo("Pseudo modifier")
+      console.log(change_pseudo)
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSaveEmail = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await fetch(`${API_URL}/account/request-email-change`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          "new_email": email
+        }),
+      });
+
+      const change_pseudo = await response.json()
+      setMessageEmail("Mail de confirmation envoyé")
+      console.log(change_pseudo)
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  return (
+    <main className="content">
+      <div className="content_top">
+        {errorMessage && <p className="alert-red">{errorMessage}</p>}
+        {message && <p className="alert-green">{message}</p>}
+        <div className="block_white">
+          <h2>Mon Profil: {username}</h2>
+        </div>
+        <br/>
+        <br/>
+        <div className="block_white">
+          {messagePseudo && <p className="alert-green">{messagePseudo}</p>}
+          <h2>Pseudo (Public name) - Changement restant {advantages?.change_pseudo}</h2>
+          <div className="profile_form">
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <input type="text" name="pseudo" value={pseudo} onChange={handleChangePseudo}/>
+              <ButtonFancy label="SAVE" onClick={handleSavePseudo} />
+            </div>
+          </div>
+        </div>
+        <br/>
+        <br/>
+        <div className="block_white">
+          {messageEmail && <p className="alert-green">{messageEmail}</p>}
+          <h2>Email</h2>
+          <div className="profile_form">
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <input type="text" name="email" value={email} onChange={handleChangeEmail}/>
+              <ButtonFancy label="SAVE" onClick={handleSaveEmail}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );  
+};
+
+export default ProfilePage;
