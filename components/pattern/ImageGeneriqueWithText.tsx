4e9c21cfce4c966693dfff277a4a75edf394ecdf
rd156 <hugo.ll@free.fr>
2025-03-07 12:40:54 +0100

    [FIX] gestion des onclicks sur image

--- components/pattern/ImageGeneriqueWithText.tsx
+++ components/pattern/ImageGeneriqueWithText.tsx
@@ -1,15 +1,15 @@
 import Image from 'next/image';
 import { useOption } from "@/context/OptionsContext";
 
-const ImageGeneriqueWithText = ({imageType, imageName, width, height, translations = null, defaultType = null, defaultName = null, className = "", alt = ""}) => {
+const ImageGeneriqueWithText = ({imageType, imageName, width, height, translations = null, defaultType = null, defaultName = null, className = "", alt = "", onClick}) => {
   const { option, getImageUrl } = useOption();
   if (imageType == null || imageName == null)
   {
-    return <Image src={getImageUrl(`${defaultType}/${defaultName}.webp`)} alt={alt} width={width} height={height} className={className}/>
+    return <Image onClick={onClick} src={getImageUrl(`${defaultType}/${defaultName}.webp`)} alt={alt} width={width} height={height} className={className}/>
   }
   else
   {
-    return <Image src={getImageUrl(`${imageType}/${imageName}.webp`)} alt={alt} width={width} height={height} className={className}/>
+    return <Image onClick={onClick} src={getImageUrl(`${imageType}/${imageName}.webp`)} alt={alt} width={width} height={height} className={className}/>
   }
 };
 
